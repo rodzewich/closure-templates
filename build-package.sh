c8939f8032c0a07a9a402d07a70faacf6bc68534
Nick Santos <nick@medium.com>
2015-06-04 12:02:27 -0400

    update release process for maven

--- build-package.sh
+++ build-package.sh
@@ -3,9 +3,8 @@
 set -ex
 
 rm -f package/SoyToJsSrcCompiler.jar package/SoyMsgExtractor.jar package/soyutils.js package/soyutils_usegoog.js
-ant clean
-ant generated-soyutils SoyToJsSrcCompiler SoyMsgExtractor
-cp build/SoyToJsSrcCompiler.jar package/
-cp build/SoyMsgExtractor.jar package/
-cp build/javascript/soyutils.js package/
-cp build/javascript/soyutils_usegoog.js package/
+mvn clean
+mvn package
+cp target/*-SoyToJsSrcCompiler.jar package/SoyToJsSrcCompiler.jar
+cp target/*-SoyMsgExtractor.jar package/SoyMsgExtractor.jar
+cp target/soyutils_usegoog.js package/
